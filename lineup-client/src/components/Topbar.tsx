d035f89382ca0ead103892b4594fb3564b8ad911
Rhyder Swen <47305744+rhyderswen@users.noreply.github.com>
2026-02-01 20:50:18 -0500

    Created Homepage with Basic Login (#6)
    * Started working on home page layout
    
    * Integrated login
    
    * Switched in Joey's logos

---
+++ lineup-client/src/components/Topbar.tsx
@@ -0,0 +1,47 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router";
+import lineupLogo from "../assets/lineup-full.png";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Topbar = ({ children }: Props) => {
+  const { isAuthenticated, logout } = useAuth0();
+
+  return (
+    <div className="root">
+      <div className="topbar">
+        <div className="lineUpLogo">
+          <Link to="/">
+            <img src={lineupLogo} alt="Line Up Logo" height={50} />
+          </Link>
+        </div>
+        <div className="signOutButton">
+          {isAuthenticated && (
+            <button
+              onClick={() =>
+                logout({
+                  logoutParams: { returnTo: globalThis.location.origin },
+                })
+              }
+              className="inlineButton"
+            >
+              Log Out
+            </button>
+          )}
+        </div>
+      </div>
+      <hr
+        style={{
+          borderColor: "var(--primary)",
+          borderWidth: "1px",
+          borderStyle: "solid",
+        }}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default Topbar;
---
+++ lineup-client/src/pages/LoggedOutHome.tsx
@@ -0,0 +1,18 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoggedOutHome = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="home">
+      Welcome to <b>Line Up</b>, your best tool for easily scheduling any
+      shift-based events. To get started,{" "}
+      <button className="inlineButton" onClick={() => loginWithRedirect()}>
+        sign in
+      </button>{" "}
+      with your Google account!
+    </div>
+  );
+};
+
+export default LoggedOutHome;
