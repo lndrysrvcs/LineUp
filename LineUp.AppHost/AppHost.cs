2df09752048840117d6014aa4152db171944a86b
lop4cwru <lop4@case.edu>
2026-03-30 16:19:55 -0400

    re-initial commit
    adding back all the changes from the earlier branches so it's consistent with main

--- LineUp.AppHost/AppHost.cs
+++ LineUp.AppHost/AppHost.cs
@@ -57,24 +57,27 @@ else
         .WaitFor(api);
 }
 
-builder.Eventing.Subscribe<ResourceEndpointsAllocatedEvent>((e, ct) => {
-    switch (e.Resource.Name)
+builder.Eventing.Subscribe<ResourceEndpointsAllocatedEvent>(
+    (e, ct) =>
     {
-        case "api":
+        switch (e.Resource.Name)
+        {
+            case "api":
             {
                 var endpoint = api.GetEndpoint("http");
                 Console.WriteLine($"Backend: {endpoint.Url}");
                 Console.WriteLine($"Scalar: {endpoint.Url}/scalar");
                 break;
             }
-        case "web":
+            case "web":
             {
                 var endpoint = web.GetEndpoint("http");
                 Console.WriteLine($"Frontend: {endpoint.Url}");
                 break;
             }
+        }
+        return Task.CompletedTask;
     }
-    return Task.CompletedTask;
-});
+);
 
 builder.Build().Run();
